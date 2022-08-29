8f1485844b7b5ef1c28e6654b3deaab21c521f6c
Filippo Ledda <filippo@metacell.us>
2022-08-29 18:23:01 +0200

    #20 Add base login logic

--- applications/areg-portal/frontend/src/App.tsx
+++ applications/areg-portal/frontend/src/App.tsx
@@ -7,6 +7,7 @@ import Navbar from "./components/NavBar/Navbar";
 import About from "./components/About";
 import Home from "./components/Home";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Submit from "./components/Submit";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
+          <Route path="/submit" component={Submit} />
         </Switch>
       </ThemeProvider>
     </BrowserRouter>
