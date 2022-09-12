b1b633e97f234d6eca789213b8db12150d798e98
afonso <afonsobspinto@gmail.com>
2022-09-12 15:58:40 +0100

    #29 feat: Add raw antibody data ingestion step

--- applications/areg-portal/backend/api/management/commands/ingest.py
+++ applications/areg-portal/backend/api/management/commands/ingest.py
@@ -6,6 +6,14 @@ from api.models import STATUS, CommercialType
 
 TRUNCATE_STM = "TRUNCATE TABLE $tableName CASCADE;"
 INSERT_INTO_STM = "INSERT INTO $tableName ($columns) VALUES {} ;"
+DROP_TABLE_STM = "DROP TABLE IF EXISTS $tableName ;"
+CREATE_TABLE_STM = "CREATE TABLE $tableName ($columns) ; "
+
+
+def get_create_table(table_name, columns):
+    columns_str = ", ".join([f"{column} text" for column in columns])
+    return CREATE_TABLE_STM.replace('$tableName', table_name) \
+        .replace('$columns', columns_str)
 
 
 def get_insert_into_table_stm(table_name, columns, entries):
@@ -20,6 +28,7 @@ def clean_empty_value(value):
 
 
 def handle_undefined_commercial_type(value):
+    # todo: handle accordingly to https://github.com/MetaCell/scicrunch-antibody-registry/issues/53
     if value not in CommercialType.labels:
         return 'other'
     return value
@@ -50,6 +59,16 @@ class Command(BaseCommand):
                                                              ['id', 'domain_name', 'vendor_id', 'status', 'link'],
                                                              len(df_vendor_domain))
 
+        # Prepare raw antibody inserts
+        tmp_table_name = 'tmp_table'
+        header = ['ab_name', 'ab_target', 'target_species', 'vendor', 'vendor_id', 'catalog_num', 'clonality',
+                  'source_organism', 'clone_id', 'url', 'link', 'ab_target_entrez_gid', 'product_isotype',
+                  'product_conjugate', 'product_form', 'target_subregion', 'target_modification', 'comments',
+                  'feedback', 'defining_citation', 'disc_date', 'curator_comment', 'id', 'ab_id', 'ab_id_old',
+                  'of_record', 'ix', 'uid', 'status', 'insert_time', 'curate_time', 'cat_alt', 'commercial_type',
+                  'uniprot_id', 'epitope'
+                  ]
+
         with transaction.atomic():
             with connection.cursor() as cursor:
                 # delete tables content (opposite order of insertion)
@@ -67,16 +86,29 @@ class Command(BaseCommand):
                 cursor.execute(vendor_insert_stm, vendor_params)
 
                 # insert vendors domains
+                # todo: Handle bad references according to https://github.com/MetaCell/scicrunch-antibody-registry/issues/54
                 vendor_domain_params = []
                 for index, row in df_vendor_domain.iterrows():
-                    # todo: Handle bad references according to https://github.com/MetaCell/scicrunch-antibody-registry/issues/54
                     vendor_domain_params.extend(
                         [row['id'], row['domain_name'], row['vendor_id'],
                          status_reverse_map[row['status']], False])
                 cursor.execute(vendor_domain_insert_stm, vendor_domain_params)
 
                 # Create copy table
-                # Insert data
+
+                cursor.execute(DROP_TABLE_STM.replace('$tableName', tmp_table_name))
+                cursor.execute(get_create_table(tmp_table_name, header))
+
+                # Insert raw data
+                for chunk in pd.read_csv(options['antibody_table_uri'], chunksize=10 ** 4, dtype=str):
+                    raw_data_insert_stm = get_insert_into_table_stm(tmp_table_name, header, len(chunk))
+                    row_params = []
+                    for index, row in chunk.iterrows():
+                        row_params.extend(row.values)
+                    cursor.execute(raw_data_insert_stm, row_params)
+
                 # Insert select distinct antigen
                 # Insert into antibody
                 # Update vendor domain link
+
+                print("Ingestion finished")
