067fbce2a8bb97f10d74d2704ca19f1730a0c177
root <root@LAPTOP-PT43ITDP.localdomain>
2022-09-13 10:25:15 +0200

    antibody details form added

--- applications/areg-portal/frontend/src/components/Submit/index.tsx
+++ applications/areg-portal/frontend/src/components/Submit/index.tsx
@@ -1,1 +1,11 @@
-export default () => "TODO"
+import { Grid } from "@mui/material";
+import React from "react";
+import AntibodyDetailsForm from "./AntibodyDetailsForm";
+const Index = () => {
+    return (
+        <Grid container sx={{ marginTop: "100px" }} justifyContent="center" alignItems="center">
+            <AntibodyDetailsForm />
+        </Grid>
+    )
+}
+export default Index;
--- applications/areg-portal/frontend/src/components/icons.tsx
+++ applications/areg-portal/frontend/src/components/icons.tsx
@@ -382,3 +382,17 @@ export const ExternalLinkIcon = (props: SvgIconProps) => (
     />
   </SvgIcon>
 );
+
+export const AlertIcon = (props: SvgIconProps) => (
+  <SvgIcon viewBox="0 0 16 16" fontSize="small" stroke={props.stroke}>
+    <path
+      xmlns="http://www.w3.org/2000/svg"
+      d="M8.00016 5.33325V7.99992M8.00016 10.6666H8.00683M14.6668 7.99992C14.6668 11.6818 11.6821 14.6666 8.00016 14.6666C4.31826 14.6666 1.3335 11.6818 1.3335 7.99992C1.3335 4.31802 4.31826 1.33325 8.00016 1.33325C11.6821 1.33325 14.6668 4.31802 14.6668 7.99992Z"
+      stroke="#F04438"
+      strokeWidth="1.33333"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      fill="none"
+    />
+  </SvgIcon>
+)
