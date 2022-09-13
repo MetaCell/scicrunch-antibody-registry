585d92f68cfdab2060728fb0798b624f76f37cbd
root <root@LAPTOP-PT43ITDP.localdomain>
2022-09-13 11:20:06 +0200

    chose the wrong parent

--- applications/areg-portal/frontend/src/components/Home/index.tsx
+++ applications/areg-portal/frontend/src/components/Home/index.tsx
@@ -1,1 +1,15 @@
-export default () => "TODO"
+import React from "react";
+
+import { Container } from "@mui/material";
+
+import AntibodiesTable from "./AntibodiesTable";
+
+const Home = () => {
+  return (
+    <Container maxWidth="xl">
+      <AntibodiesTable />
+    </Container>
+  );
+};
+
+export default Home;
--- applications/areg-portal/frontend/src/components/Submit/index.tsx
+++ applications/areg-portal/frontend/src/components/Submit/index.tsx
@@ -1,11 +1,1 @@
-import { Grid } from "@mui/material";
-import React from "react";
-import AntibodyDetailsForm from "./AntibodyDetailsForm";
-const Index = () => {
-    return (
-        <Grid container sx={{ marginTop: "100px" }} justifyContent="center" alignItems="center">
-            <AntibodyDetailsForm />
-        </Grid>
-    )
-}
-export default Index;
+export default () => "TODO"
