b5ff56a0db2c7f267a623698693ccbd54d3587f9
Filippo Ledda <filippo@metacell.us>
2022-09-23 18:38:42 +0200

    #33 #35 Add general info api stub

--- applications/portal/frontend/src/rest/api.ts
+++ applications/portal/frontend/src/rest/api.ts
@@ -649,6 +649,25 @@ export const AntibodyAllOfStatusEnum = {
 export type AntibodyAllOfStatusEnum = typeof AntibodyAllOfStatusEnum[keyof typeof AntibodyAllOfStatusEnum];
 
 /**
+ * Information about the data in the system
+ * @export
+ * @interface DataInfo
+ */
+export interface DataInfo {
+    /**
+     * The total number of antibodies in the system
+     * @type {number}
+     * @memberof DataInfo
+     */
+    'total': number;
+    /**
+     * 
+     * @type {string}
+     * @memberof DataInfo
+     */
+    'lastupdate': string;
+}
+/**
  * The search request body that allows filtering combinations over multiple columns
  * @export
  * @interface FilterRequest
@@ -1219,6 +1238,100 @@ export class AntibodyApi extends BaseAPI {
      */
     public updateAntibody(antibodyId: number, addUpdateAntibody: AddUpdateAntibody, options?: AxiosRequestConfig) {
         return AntibodyApiFp(this.configuration).updateAntibody(antibodyId, addUpdateAntibody, options).then((request) => request(this.axios, this.basePath));
+    }
+}
+
+
+/**
+ * DefaultApi - axios parameter creator
+ * @export
+ */
+export const DefaultApiAxiosParamCreator = function (configuration?: Configuration) {
+    return {
+        /**
+         * 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        datainfoGet: async (options: AxiosRequestConfig = {}): Promise<RequestArgs> => {
+            const localVarPath = `/datainfo`;
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+
+            const localVarRequestOptions = { method: 'GET', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+
+    
+            setSearchParams(localVarUrlObj, localVarQueryParameter);
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+
+            return {
+                url: toPathString(localVarUrlObj),
+                options: localVarRequestOptions,
+            };
+        },
+    }
+};
+
+/**
+ * DefaultApi - functional programming interface
+ * @export
+ */
+export const DefaultApiFp = function(configuration?: Configuration) {
+    const localVarAxiosParamCreator = DefaultApiAxiosParamCreator(configuration)
+    return {
+        /**
+         * 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async datainfoGet(options?: AxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<DataInfo>> {
+            const localVarAxiosArgs = await localVarAxiosParamCreator.datainfoGet(options);
+            return createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration);
+        },
+    }
+};
+
+/**
+ * DefaultApi - factory interface
+ * @export
+ */
+export const DefaultApiFactory = function (configuration?: Configuration, basePath?: string, axios?: AxiosInstance) {
+    const localVarFp = DefaultApiFp(configuration)
+    return {
+        /**
+         * 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        datainfoGet(options?: any): AxiosPromise<DataInfo> {
+            return localVarFp.datainfoGet(options).then((request) => request(axios, basePath));
+        },
+    };
+};
+
+/**
+ * DefaultApi - object-oriented interface
+ * @export
+ * @class DefaultApi
+ * @extends {BaseAPI}
+ */
+export class DefaultApi extends BaseAPI {
+    /**
+     * 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof DefaultApi
+     */
+    public datainfoGet(options?: AxiosRequestConfig) {
+        return DefaultApiFp(this.configuration).datainfoGet(options).then((request) => request(this.axios, this.basePath));
     }
 }
 
