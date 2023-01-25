e32960043bbdef7346484e5d8b219b62b44b4bce
Filippo Ledda <filippo@metacell.us>
2023-01-25 12:32:09 +0100

    Fix auth

--- applications/portal/backend/main.py
+++ applications/portal/backend/main.py
@@ -78,7 +78,7 @@ if os.environ.get('KUBERNETES_SERVICE_HOST', None):
 
 # enable the Bearer Authentication
 security = APIKeyCookie(name="kc-access") # This works but having issues with dev server
-# security = HTTPBearer()
+security = HTTPBearer()
 
 
 async def has_access(credentials: HTTPBasicCredentials = Depends(security)):
