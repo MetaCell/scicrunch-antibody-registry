c58d54f853d2beb7072f54a93c98fe184578b810
Filippo Ledda <filippo@metacell.us>
2023-03-10 19:57:49 +0100

    #128 Message for no public antibody

--- applications/portal/frontend/src/components/Submit/DuplicatedMsg.tsx
+++ applications/portal/frontend/src/components/Submit/DuplicatedMsg.tsx
@@ -61,18 +61,23 @@ const DuplicatedMsg = (props) => {
         <Typography variant="subtitle1" align="center" sx={classes.message}>
           The Antibody you Entered is a Duplicate, which will be Rejected. We
           have found that the antibody you entered already exists in our system.
-          The ID associated with the antibody is RRID:AB_
-          {props.antibodyId}.
+          The ID associated with the antibody is 
+          
+        </Typography>
+        <Typography variant="subtitle1" align="center" sx={{ ...classes.message, fontWeight: 700, fontSize: "1em" }}>
+          RRID:AB_{props.antibodyId}.
         </Typography>
         <Divider>
           <Typography variant="subtitle1" sx={classes.message}>
             Chinese
           </Typography>
         </Divider>
-        <Typography variant="subtitle1" align="center" sx={classes.message}>
+        <Typography variant="subtitle1" component="p" align="center" sx={classes.message}>
           您所提交的抗体已经存在于我们的系统中，此次申请将会被拒绝
-          我们发现您所提交的抗体已经存在于我们的系统中，它的ID 是 RRID:AB_
-          {props.antibodyId}.
+          我们发现您所提交的抗体已经存在于我们的系统中，它的ID 是
+        </Typography>
+        <Typography variant="subtitle1" align="center" sx={{ ...classes.message, fontWeight: 700, fontSize: "1em" }}>
+          RRID:AB_{props.antibodyId}.
         </Typography>
         <Box>
           <Button
