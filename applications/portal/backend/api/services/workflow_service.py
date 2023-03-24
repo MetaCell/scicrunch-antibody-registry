4a6b1ff2aa8c5e9d990fcb59f1d60fc3aff3aada
Filippo Ledda <filippo@metacell.us>
2023-03-24 11:03:19 +0100

    Keep ingestion pods more time

--- applications/portal/backend/api/services/workflow_service.py
+++ applications/portal/backend/api/services/workflow_service.py
@@ -14,7 +14,13 @@ def _create_task(image_name, **kwargs):
     )
 
 
+
 def execute_ingestion_workflow(file_id: str):
+    ttl_strategy={
+    'secondsAfterCompletion': 60 * 60 * 24,
+    'secondsAfterSuccess': 60 * 20,
+    'secondsAfterFailure': 60 * 60 * 24
+    }       
     operations.PipelineOperation(
         basename=INGEST_OP,
         tasks=(
@@ -23,4 +29,5 @@ def execute_ingestion_workflow(file_id: str):
                 command=["python", "manage.py", "ingest", file_id]
             ),
         ),
+        ttl_strategy=ttl_strategy
     ).execute()
