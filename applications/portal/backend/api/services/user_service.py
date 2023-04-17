c21aff750ca1ff6df46f8e77ccb790a227c78e2b
Filippo Ledda <filippo@metacell.us>
2023-04-17 19:05:05 +0200

    #146 fix backoffice submitter name

--- applications/portal/backend/api/services/user_service.py
+++ applications/portal/backend/api/services/user_service.py
@@ -5,7 +5,7 @@ class UnrecognizedUser(Exception):
     pass
 
 def get_current_user_id() -> str:
-    token = get_authentication_token()
+    token = get_authentication_token().replace("Bearer ", "")
     try:
         return jwt.decode(token, options={"verify_signature": False}, algorithms='RS256')['sub']
     except Exception as e:
