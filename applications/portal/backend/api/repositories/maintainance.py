61b9fa0e395eeac86a6649447d48a63cc8fe7278
Filippo Ledda <filippo@metacell.us>
2023-05-18 11:38:55 +0200

    Materialized view update fix

--- applications/portal/backend/api/repositories/maintainance.py
+++ applications/portal/backend/api/repositories/maintainance.py
@@ -1,6 +1,9 @@
+from django.db import connection
+
+
 def refresh_search_view():
     import threading
     def refresh_search_view_thread():
-        cursor = connection.cursor()
-        cursor.execute("REFRESH MATERIALIZED VIEW CONCURRENTLY antibody_search;")
+        with connection.cursor() as cursor:
+            cursor.execute("REFRESH MATERIALIZED VIEW CONCURRENTLY antibody_search;")
     threading.Thread(target=refresh_search_view_thread).start()
