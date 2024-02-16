ad4b675f174d34d7e470c3b7407584b573e893f4
D. Gopal Krishna <nikhil0223@gmail.com>
2024-02-16 16:30:15 +0530

    #96 rechunk_catalog_number for migration

--- applications/portal/backend/api/repositories/maintainance.py
+++ applications/portal/backend/api/repositories/maintainance.py
@@ -1,5 +1,7 @@
 from django.db import connection
 from cloudharness import log
+from api.utilities.functions import catalog_number_chunked
+
 
 def refresh_search_view():
     import threading
@@ -9,3 +11,13 @@ def refresh_search_view():
             cursor.execute("REFRESH MATERIALIZED VIEW CONCURRENTLY antibody_search;")
         connection.commit()
     threading.Thread(target=refresh_search_view_thread).start()
+
+
+def rechunk_catalog_number(apps, schema_editor):
+    Antibody = apps.get_model('api', 'Antibody')
+    with connection.cursor() as cursor:
+        for antibody in Antibody.objects.all().values('ix', 'catalog_num_search', 'catalog_num', 'cat_alt'):
+            new_catalog_number_chunked = catalog_number_chunked(antibody['catalog_num'], antibody['cat_alt'])
+            if new_catalog_number_chunked != antibody['catalog_num_search']:
+                cursor.execute(f"UPDATE api_antibody SET catalog_num_search = {new_catalog_number_chunked} WHERE ix={antibody['ix']};") 
+    connection.commit()
