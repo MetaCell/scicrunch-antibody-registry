c9b80c7925b9c21bbbe43d60f5cf807602339719
D. Gopal Krishna <nikhil0223@gmail.com>
2024-04-09 17:04:24 +0530

    #121 cronjob function add support to take secrets

--- applications/portal/backend/api/management/commands/scicrunch_citation.py
+++ applications/portal/backend/api/management/commands/scicrunch_citation.py
@@ -7,7 +7,9 @@ scricrunch API - https://api.scicrunch.io/elastic/v1/RIN_Tool_pr/_search?q=AB_90
 import requests
 from api.models import Antibody, STATUS
 from django.core.management.base import BaseCommand
+from cloudharness.utils.secrets import get_secret
 
+scicrunch_api_key = get_secret("API_KEY")
 
 def get_json_body(ab_id):
     return {
@@ -42,14 +44,15 @@ class Command(BaseCommand):
     help = "Ingests citation metric from scicrunch"
 
     def add_arguments(self, parser):
-        parser.add_argument("api_key", type=str)
+        pass
 
     def handle(self, *args, **options):
-        api_key = options["api_key"]
         antibodies_ids = Antibody.objects.filter(status=STATUS.CURATED).values_list(
             "ab_id", flat=True
         )
-        citation_metrics = self.fetch_scicrunch_citation_metric(antibodies_ids, api_key)
+        citation_metrics = self.fetch_scicrunch_citation_metric(
+            antibodies_ids, scicrunch_api_key
+        )
         if citation_metrics:
             self.ingest_scicrunch_citation_metric(citation_metrics)
             self.stdout.write(
@@ -60,31 +63,39 @@ class Command(BaseCommand):
         else:
             self.stdout.write(self.style.ERROR(f"Failed to ingest citation metrics"))
 
-    def fetch_scicrunch_citation_metric(self, antibody_ids, api_key):
+
+    def fetch_scicrunch_citation_metric(self, antibody_ids, scicrunch_api_key):
         citation_metrics = []
         # TODO: Add a rate limiter - The 10 requests per second (rps) baseline
         for ab_id in antibody_ids:
             abid = "AB_" + str(ab_id)
             # Below is the link to get the mentions
-            # link_for_api = f"https://api.scicrunch.io/elastic/v1/RIN_Tool_pr/_search?q={abid}&key={API_KEY}"
-            link_for_api = f"https://scicrunch.org/api/1/elastic/RIN_Mentions_pr/data/_search?key={api_key}"
+            # link_for_api = f"https://api.scicrunch.io/elastic/v1/RIN_Tool_pr/_search?q={abid}&key={scicrunch_api_key}"
+            link_for_api = f"https://scicrunch.org/api/1/elastic/RIN_Mentions_pr/data/_search?key={scicrunch_api_key}"
             json_body = get_json_body(abid)
-            response = requests.post(
-                link_for_api,
-                json=json_body,
-                headers={"Content-Type": "application/json"},
-            )
-            if response.status_code == 200:
-                res = response.json()
-                # Below is the old code to get the mentions
-                # citation_count = res["hits"]["hits"][0]["_source"]["mentions"][0][
-                #     "totalMentions"
-                # ]["count"]
-                citation_count = res["hits"]["total"]
+            try:
+                response = requests.post(
+                    link_for_api,
+                    json=json_body,
+                    headers={"Content-Type": "application/json"},
+                )
+                if response.status_code == 200:
+                    res = response.json()
+                    # Below is the old code to get the mentions
+                    # citation_count = res["hits"]["hits"][0]["_source"]["mentions"][0][
+                    #     "totalMentions"
+                    # ]["count"]
+                    citation_count = res["hits"]["total"]
 
-                citation_metrics.append({ab_id: citation_count})
-            else:
-                self.stdout.write(self.style.ERROR(f"Failed to fetch citation metrics"))
+                    citation_metrics.append({ab_id: citation_count})
+                else:
+                    self.stdout.write(
+                        self.style.ERROR(f"Failed to fetch citation metrics")
+                    )
+            except Exception as e:
+                self.stdout.write(
+                    self.style.ERROR(f"Failed to fetch citation metrics for {ab_id}")
+                )
         return citation_metrics
 
     def ingest_scicrunch_citation_metric(self, citation_metrics):
