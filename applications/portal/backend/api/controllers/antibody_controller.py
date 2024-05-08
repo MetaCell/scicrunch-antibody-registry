49790420cfd06b76d947676b355e3fbf00253f13
D. Gopal Krishna <nikhil0223@gmail.com>
2024-05-08 19:30:16 +0530

    AREG-120 controllers update

--- applications/portal/backend/api/controllers/antibody_controller.py
+++ applications/portal/backend/api/controllers/antibody_controller.py
@@ -3,7 +3,7 @@ from typing import List, Union
 import os
 
 from cloudharness import log
-from api.models import Antibody
+from api.models import Antibody, STATUS
 from api.services.user_service import UnrecognizedUser, get_current_user_id
 
 
@@ -100,3 +100,11 @@ def get_antibodies_export():
         generate_antibodies_csv_file(fname)
     return RedirectResponse("/" + fname)
     # return FileResponse(fname, filename="antibodies_export.csv")
+
+def get_curated_antibodies():
+    antibodies_ids = Antibody.objects.filter(status=STATUS.CURATED).values_list(
+        "ab_id", flat=True
+    )
+    if not antibodies_ids:
+        raise HTTPException(status_code=404, detail="No Antibodies found with status CURATED")
+    return antibodies_ids
--- applications/portal/backend/api/controllers/ingest_controller.py
+++ applications/portal/backend/api/controllers/ingest_controller.py
@@ -3,6 +3,9 @@ from fastapi import HTTPException, status
 from api.services.workflow_service import execute_ingestion_workflow
 from openapi.models import IngestRequest
 from api.services.keycloak_service import KeycloakService
+from api.models import Antibody, STATUS
+from api.utilities.exceptions import AntibodyDoesNotExist
+
 
 def ingest(body: IngestRequest):
     auth = KeycloakService()
@@ -15,3 +18,16 @@ def ingest(body: IngestRequest):
                                 detail="You are not authorized to ingest data.")
     execute_ingestion_workflow(body.driveLinkOrId, body.hot)
     return status.HTTP_200_OK
+
+
+def ingest_scicrunch_citation_metric(antibody_id, number_of_citation):
+    try:
+        antibodies_filtered_by_id = Antibody.objects.filter(ab_id=antibody_id)
+        if not antibodies_filtered_by_id:
+            raise Antibody.DoesNotExist
+        for antibody in antibodies_filtered_by_id:
+            antibody.citation = number_of_citation
+            antibody.save()
+        return status.HTTP_200_OK
+    except AntibodyDoesNotExist as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
