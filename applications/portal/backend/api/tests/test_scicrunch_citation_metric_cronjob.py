0e921564ee5c9b0d1070e66d7d8a0b9eb2a3de25
D. Gopal Krishna <nikhil0223@gmail.com>
2024-05-08 19:30:57 +0530

    AREG-120 test scricunch django Command and  Rate limiter

--- applications/portal/backend/api/tests/test_scicrunch_citation_metric_cronjob.py
+++ applications/portal/backend/api/tests/test_scicrunch_citation_metric_cronjob.py
@@ -1,8 +1,12 @@
 from django.test import TestCase
 from api.models import Antibody, STATUS
-from api.management.commands.scicrunch_citation import get_json_body, Command
+from api.management.commands.scicrunch_citation import Command
 from api.services.antibody_service import *
-from .data.test_data import example_ab
+from unittest.mock import patch
+from api.utilities.exceptions import FetchCitationMetricFailed
+from api.services.ingest_service import RateLimiter
+import time
+from api.tests.data.test_data import TEST_ANTIBODIES_FOR_SCICRUNCH_CITATION
 
 
 class ScicrunchCitationMetricCronJobTests(TestCase):
@@ -16,17 +20,57 @@ class ScicrunchCitationMetricCronJobTests(TestCase):
         https://scicrunch.org/resources/data/record/nif-0000-07730-1/AB_90755/resolver
         """
         command = Command()
-        ab = Antibody.objects.create(
-            ab_id="90755", ab_name="test_ab_name", status=STATUS.CURATED
-        )
-        ab.save()
 
-        # check that the citation of this should be null - before script
-        self.assertIsNone(ab.citation)
+        # run the command before adding the antibody - this should fail... since the antibody is not present in the database
+        try:
+            command.handle()
+        except SystemExit as e:
+            self.assertEqual(e.code, 1)
+
+        for testab in TEST_ANTIBODIES_FOR_SCICRUNCH_CITATION[:4]:
+            ab = Antibody.objects.create(
+                ab_id=testab["ab_id"],
+                ab_name=testab["ab_name"],
+                status=STATUS.CURATED
+            )
+            ab.save()
 
         # run the command
         command.handle()
 
-        # now it should be 137 - after the script
-        a = Antibody.objects.get(ab_id=ab.ab_id)
-        self.assertEqual(a.citation, 137)
+        # now check for all the antibodies - if the number of citation is as expected. 
+        for testab in TEST_ANTIBODIES_FOR_SCICRUNCH_CITATION[:4]:
+            a = Antibody.objects.get(ab_id=testab["ab_id"])
+            self.assertEqual(
+                a.citation, 
+                testab["expected_citation"]
+            )
+        
+        unknown_id_antibody = TEST_ANTIBODIES_FOR_SCICRUNCH_CITATION[4]
+        ab2 = Antibody.objects.create(
+            ab_id=unknown_id_antibody["ab_id"],  ## unknown Id [100]
+            ab_name=unknown_id_antibody["ab_name"],
+            status=STATUS.CURATED
+        )
+        ab2.save()
+
+        command.handle()
+        
+        a2 = Antibody.objects.get(ab_id=unknown_id_antibody["ab_id"])
+        self.assertEqual(a2.citation, 0)      ## 0 - since it doesn't find any citation for unknown Id 1 
+
+
+    def test_rate_limiter(self):
+        limiter = RateLimiter(max_requests_per_second=10)
+        
+        # test if allows 10 requests per second
+        t1 = time.time()
+        for i in range(1, 28):
+            if i == 11:
+                t2 = time.time()
+            limiter.add_request()
+        t3 = time.time()
+        self.assertGreaterEqual(t2-t1, 1)
+        self.assertGreaterEqual(t3-t1, 2)
+        
+
