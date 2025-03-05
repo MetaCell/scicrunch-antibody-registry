3b94f5bc2f61e63f741905c8b7d0b9ef40f1e03d
D. Gopal Krishna <nikhil0223@gmail.com>
2025-03-05 15:19:35 +0530

    AREG-105 - ttl_cache util to keep cache in memory

---
+++ applications/portal/backend/api/utilities/cache.py
@@ -0,0 +1,32 @@
+from functools import lru_cache, update_wrapper
+from typing import Callable, Any
+from math import floor
+import time
+
+
+# TTL Cache Decorator
+def ttl_cache(maxsize: int = 128, typed: bool = False, ttl: int = -1):
+    if ttl <= 0:
+        ttl = 65536
+
+    hash_gen = _ttl_hash_gen(ttl)
+    def wrapper(func: Callable) -> Callable:
+        @lru_cache(maxsize, typed)
+        def ttl_func(ttl_hash, *args, **kwargs):
+            return func(*args, **kwargs)
+
+        def wrapped(*args, **kwargs) -> Any:
+            th = next(hash_gen)
+            print("th from ttl_cache: ", th, func)
+            return ttl_func(th, *args, **kwargs)
+
+        return update_wrapper(wrapped, func)
+
+    return wrapper
+
+
+# Time-to-Live Hash Generator
+def _ttl_hash_gen(seconds: int):
+    start_time = time.time()
+    while True:
+        yield floor((time.time() - start_time) / seconds)
