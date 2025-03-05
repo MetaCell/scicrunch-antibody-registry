cef3e4e62df73c4c6f81aa4a15a9df084ca947da
D. Gopal Krishna <nikhil0223@gmail.com>
2025-03-05 16:23:22 +0530

    AREG-105 - fix unit tests

--- applications/portal/backend/api/tests/__init__.py
+++ applications/portal/backend/api/tests/__init__.py
@@ -1,5 +1,6 @@
 from .test_vendor_admin import VendorAdminTests
 from .test_search_filter import SearchAndFilterAntibodiesTestCase
 from .test_antibodies import AntibodiesTestCase
+from .test_ttl_cache import TTLCacheTestCase
 import os
 os.environ["TEST"] = "True"
