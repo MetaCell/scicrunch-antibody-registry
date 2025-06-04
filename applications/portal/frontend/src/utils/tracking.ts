2cf05741415135b2772d7676455ad070dcd1ff1d
Filippo Ledda <filippo@metacell.us>
2025-06-04 11:42:47 +0200

    Tweak analytics event on vendor click

--- applications/portal/frontend/src/utils/tracking.ts
+++ applications/portal/frontend/src/utils/tracking.ts
@@ -4,7 +4,8 @@ declare const window: any;
 export function trackVendorClick(vendorName: string) {
   return () => {
     if (window.gtag) {
-      window.gtag('event', 'vendor_click', {
+      window.gtag('event', 'click', {
+        event_category: 'Vendor clicks',
         vendor_name: vendorName, // Track the vendor name
         value: vendorName,
       });
